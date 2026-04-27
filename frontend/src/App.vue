0606be68c9d2f90270b0680f4dd1e4e3b28a2e73
yifengjob <yifengjob@qq.com>
2026-04-27 10:14:59 +0800

    feat: 使用 vite-plugin-svg-icons 实现 SVG Sprite

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,9 +22,7 @@
         :disabled="isScanning"
         title="开始扫描选中的目录"
       >
-        <svg class="btn-icon" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-          <path fill="currentColor" fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1m3.901 7L6 4.066v7.868z" clip-rule="evenodd" />
-        </svg>
+        <svg class="btn-icon"><use href="#icon-play" /></svg>
         <span>{{ isScanning ? '扫描中...' : '开始扫描' }}</span>
       </button>
       <button 
@@ -33,9 +31,7 @@
         :disabled="!isScanning"
         title="取消当前扫描任务"
       >
-        <svg class="btn-icon" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-          <path fill="currentColor" fill-rule="evenodd" d="M256 469.334c117.821 0 213.334-95.513 213.334-213.334c0-117.82-95.513-213.333-213.334-213.333C138.18 42.667 42.667 138.18 42.667 256c0 117.821 95.513 213.334 213.333 213.334m-64-298.667h42.667v170.667H192zm85.334 0H320v170.667h-42.666z" />
-        </svg>
+        <svg class="btn-icon"><use href="#icon-pause" /></svg>
         <span>取消</span>
       </button>
       <button 
@@ -44,18 +40,14 @@
         :disabled="scanResults.length === 0"
         :title="scanResults.length === 0 ? '暂无扫描结果，无法导出' : '导出报告'"
       >
-        <svg class="btn-icon" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-          <path fill="currentColor" d="M24.086 20.904c-1.805 3.113-5.163 5.212-9.023 5.22A10.45 10.45 0 0 1 4.625 15.688A10.45 10.45 0 0 1 15.063 5.25c3.86.007 7.216 2.105 9.022 5.218l3.962 2.284l.143.082C26.88 6.784 21.504 2.25 15.063 2.248C7.64 2.25 1.625 8.265 1.623 15.688c.003 7.42 6.018 13.435 13.44 13.437c6.442-.002 11.82-4.538 13.127-10.59l-.14.082zm4.314-5.216l-7.15-4.13v2.298H10.275v3.66H21.25v2.298z" />
-        </svg>
+        <svg class="btn-icon"><use href="#icon-export" /></svg>
       </button>
       <button 
         class="btn btn-icon-only" 
         @click="showSettings = true"
         title="打开设置"
       >
-        <svg class="btn-icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-          <path fill="currentColor" d="M512.5 390.6c-29.9 0-57.9 11.6-79.1 32.8c-21.1 21.2-32.8 49.2-32.8 79.1s11.7 57.9 32.8 79.1c21.2 21.1 49.2 32.8 79.1 32.8s57.9-11.7 79.1-32.8c21.1-21.2 32.8-49.2 32.8-79.1s-11.7-57.9-32.8-79.1a110.96 110.96 0 0 0-79.1-32.8m412.3 235.5l-65.4-55.9c3.1-19 4.7-38.4 4.7-57.7s-1.6-38.8-4.7-57.7l65.4-55.9a32.03 32.03 0 0 0 9.3-35.2l-.9-2.6a442.5 442.5 0 0 0-79.6-137.7l-1.8-2.1a32.12 32.12 0 0 0-35.1-9.5l-81.2 28.9c-30-24.6-63.4-44-99.6-57.5l-15.7-84.9a32.05 32.05 0 0 0-25.8-25.7l-2.7-.5c-52-9.4-106.8-9.4-158.8 0l-2.7.5a32.05 32.05 0 0 0-25.8 25.7l-15.8 85.3a353.4 353.4 0 0 0-98.9 57.3l-81.8-29.1a32 32 0 0 0-35.1 9.5l-1.8 2.1a445.9 445.9 0 0 0-79.6 137.7l-.9 2.6c-4.5 12.5-.8 26.5 9.3 35.2l66.2 56.5c-3.1 18.8-4.6 38-4.6 57c0 19.2 1.5 38.4 4.6 57l-66 56.5a32.03 32.03 0 0 0-9.3 35.2l.9 2.6c18.1 50.3 44.8 96.8 79.6 137.7l1.8 2.1a32.12 32.12 0 0 0 35.1 9.5l81.8-29.1c29.8 24.5 63 43.9 98.9 57.3l15.8 85.3a32.05 32.05 0 0 0 25.8 25.7l2.7.5a448.3 448.3 0 0 0 158.8 0l2.7-.5a32.05 32.05 0 0 0 25.8-25.7l15.7-84.9c36.2-13.6 69.6-32.9 99.6-57.5l81.2 28.9a32 32 0 0 0 35.1-9.5l1.8-2.1c34.8-41.1 61.5-87.4 79.6-137.7l.9-2.6c4.3-12.4.6-26.3-9.5-35m-412.3 52.2c-97.1 0-175.8-78.7-175.8-175.8s78.7-175.8 175.8-175.8s175.8 78.7 175.8 175.8s-78.7 175.8-175.8 175.8" />
-        </svg>
+        <svg class="btn-icon"><use href="#icon-setting" /></svg>
       </button>
       <button 
         class="btn btn-icon-only theme-toggle" 
@@ -63,13 +55,9 @@
         :title="getThemeTooltip()"
       >
         <!-- 跟随系统主题 -->
-        <svg v-if="currentTheme === 'system'" class="btn-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path fill="currentColor" d="M7.5 2c-1.79 1.15-3 3.18-3 5.5s1.21 4.35 3.03 5.5C4.46 13 2 10.54 2 7.5A5.5 5.5 0 0 1 7.5 2m11.57 1.5l1.43 1.43L4.93 20.5L3.5 19.07zm-6.18 2.43L11.41 5L9.97 6l.42-1.7L9 3.24l1.75-.12l.58-1.65L12 3.1l1.73.03l-1.35 1.13zm-3.3 3.61l-1.16-.73l-1.12.78l.34-1.32l-1.09-.83l1.36-.09l.45-1.29l.51 1.27l1.36.03l-1.05.87zM19 13.5a5.5 5.5 0 0 1-5.5 5.5c-1.22 0-2.35-.4-3.26-1.07l7.69-7.69c.67.91 1.07 2.04 1.07 3.26m-4.4 6.58l2.77-1.15l-.24 3.35zm4.33-2.7l1.15-2.77l2.2 2.54zm1.15-4.96l-1.14-2.78l3.34.24zM9.63 18.93l2.77 1.15l-2.53 2.19z" />
-        </svg>
+        <svg v-if="currentTheme === 'system'" class="btn-icon"><use href="#icon-system-theme" /></svg>
         <!-- 浅色/深色主题 -->
-        <svg v-else class="btn-icon" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-          <path fill="currentColor" fill-rule="evenodd" d="M277.333 405.333v85.333h-42.667v-85.333zm99.346-58.824l60.34 60.34l-30.17 30.17l-60.34-60.34zm-241.359 0l30.17 30.17l-60.34 60.34l-30.17-30.17zM256 139.353c64.422 0 116.647 52.224 116.647 116.647c0 64.422-52.225 116.647-116.647 116.647A116.427 116.427 0 0 1 139.352 256c0-64.423 52.225-116.647 116.648-116.647m0 42.666c-40.859 0-73.981 33.123-73.981 74.062a73.76 73.76 0 0 0 21.603 52.296c13.867 13.867 32.685 21.64 52.378 21.603zm234.666 52.647v42.667h-85.333v-42.667zm-384 0v42.667H21.333v-42.667zM105.15 74.98l60.34 60.34l-30.17 30.17l-60.34-60.34zm301.7 0l30.169 30.17l-60.34 60.34l-30.17-30.17zM277.332 21.333v85.333h-42.667V21.333z" />
-        </svg>
+        <svg v-else class="btn-icon"><use href="#icon-light-dark" /></svg>
       </button>
     </div>
 
@@ -157,12 +145,8 @@ import { startScan, cancelScan, loadConfig, onScanProgress, onScanResult, onScan
 import { applyTheme, loadTheme, watchSystemTheme } from './utils/theme'
 import type { ThemeMode } from './utils/theme'
 
-// 导入 SVG 图标（不再需要，已改为内联）
-// import playIcon from '@/assets/play.svg'
-// import pauseIcon from '@/assets/pause.svg'
-// import exportIcon from '@/assets/export.svg'
-// import settingIcon from '@/assets/setting.svg'
-// import lightDarkIcon from '@/assets/light-dark.svg'
+// 不再需要导入 SVG 文件
+// 插件会自动将 src/assets 下的 SVG 转换为 sprite
 
 const appStore = useAppStore()
 const { isScanning, scannedCount, sensitiveFilesCount, errorCount, totalSensitiveItems, config, scanResults } = storeToRefs(appStore)
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import './style.css'
 import { applyTheme, loadTheme } from './utils/theme'
+// 导入 SVG Sprite 虚拟模块（自动生成）
+import 'virtual:svg-icons-register'
 
 // 初始化主题
 const initialTheme = loadTheme()
